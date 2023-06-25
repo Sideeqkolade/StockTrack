8a7f3a2e249c1117a99f5bbc88f6094105708887
Sideeqkolade <sideeqkolade@gmail.com>
2023-06-25 23:53:31 +0100

    adjusted nav

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def main():
     return render_template('main.html')
 
 @app.route('/dashboard')
 def display_dash():
-    return render_template('dashboard.html')
+    title = 'Dashboard'
+    return render_template('dashboard.html', title=title)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
