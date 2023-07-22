d950b5e81b0ccd66e28300ba31b699c4feba0907
Sideeqkolade <sideeqkolade@gmail.com>
2023-07-22 12:03:18 +0100

    arrange files

--- setup_dev.sql
+++
@@ -1,7 +0,0 @@
--- create a database named stocktrack_db
-
-CREATE DATABASE IF NOT EXISTS stocktrack_db;
-CREATE USER IF NOT EXISTS 'stocktrack_usr'@'localhost' IDENTIFIED BY 'stocktrack_usr_pwd';
-GRANT ALL PRIVILEGES ON `stocktrack_db`.* TO 'stocktrack_usr'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'stocktrack_usr'@'localhost';
-FLUSH PRIVILEGES;
